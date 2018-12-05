05c59b49769905459cf9c40fa035ffd981a86359
Patrick Sodré <psodre@gmail.com>
2018-12-05 06:23:49 +0000

    singularity 3.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+# Turn the work-folder into GOPATH
+export GOPATH=${SRC_DIR}
+export PATH=${GOPATH}/bin:$PATH
+pushd src/github.com/sylabs/${PKG_NAME}
 
-./configure --prefix="${PREFIX}"
-make
-make install
+# bootstrap go dependencies
+go get -u github.com/golang/dep/cmd/dep
+
+# Git Initialize
+# Apps tend to use git info to create version string
+git init
+git config --local user.email "conda@conda-forge.github.io"
+git config --local user.name "conda-forge"
+
+echo $PKG_VERSION >> .conda_version
+git add .conda_version
+git commit -m "conda build of $PKG_NAME-v$PKG_VERSION"
+git tag v${PKG_VERSION}
+
+
+
+# configure
+./mconfig \
+  -v \
+  -p $PREFIX \
+  -c "$CC" \
+  -C "$CC" \
+  -x "$CPP" \
+  -x "$CPP"
+
+# build
+make -C builddir
+
+# install
+make -C builddir install
