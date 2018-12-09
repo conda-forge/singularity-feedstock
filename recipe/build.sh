0d6bcc2ebdd4c64c0823cfde0abf2d52bb2a7b51
Patrick Sodré <psodre@gmail.com>
2018-12-09 15:03:07 +0000

    Use conda provided cni plugins

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euf
+
 export PATH=${GOPATH}/bin:$PATH
 pushd src/github.com/sylabs/${PKG_NAME}
 
@@ -9,14 +11,23 @@ export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 # configure
 ./mconfig \
   -v \
+  -s \
+  -S \
   -p $PREFIX \
   -c "$CC" \
   -C "$CC" \
   -x "$CPP" \
   -x "$CPP"
 
+pushd builddir
 # build
-make -C builddir
+make 
 
 # install
-make -C builddir install
+make install
+
+# Move Singularity Network configuration files to PREFIX/etc/cni/net.d
+mkdir -p $PREFIX/etc/cni/net.d
+for i in `find $PREFIX/etc/singularity/network -maxdepth 1 -type f`; do
+  mv $i $PREFIX/etc/cni/net.d
+done
