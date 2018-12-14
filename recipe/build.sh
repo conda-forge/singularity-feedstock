99fa967a89fee3278b4b0fddfdfc5c4cdf314b6e
Patrick Sodré <psodre@gmail.com>
2018-12-14 23:52:27 +0000

    iterate.... should build on both

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,27 +10,29 @@ go get -u github.com/golang/dep/cmd/dep
 # Create a C and CPP compiler for singularity
 cat > singularity-cc <<_EOF
 #!/usr/bin/env bash
-exec $CC -I${PREFIX}/include -L${PREFIX}/lib ${CFLAGS} ${LDFLAGS} \$@
+exec $CC -I${PREFIX}/include -L${PREFIX}/lib \$@
 _EOF
 chmod 755 singularity-cc
 
 cat > singularity-cxx <<_EOF
 #!/usr/bin/env bash
-exec $CXX -I${PREFIX}/include -L${PREFIX}/lib ${CXXFLAGS} ${LDFLAGS} \$@
+exec $CXX -I${PREFIX}/include -L${PREFIX}/lib \$@
 _EOF
 chmod 755 singularity-cxx
 
 # configure
 ./mconfig \
-  -v \
   -p $PREFIX \
   -c "${PWD}/singularity-cc" \
   -x "${PWD}/singularity-cxx"
 
 # build
 pushd builddir
-export CGO_CPPFLAGS=${CPPFLAGS}
-export CGO_LDFLAGS=${LDFLAGS}
+export CGO_CFLAGS="${CFLAGS}"
+export CGO_CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CGO_LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+export LD_LIBRARY_PATH=${PREFIX}/lib
 make
 
 # install
