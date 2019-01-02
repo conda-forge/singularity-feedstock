f3aae54ae286d24ef5b3cd375882a11df1d50653
Patrick Sodré <psodre@gmail.com>
2019-01-02 06:08:04 +0000

    fix grammar in post-link.sh

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -6,7 +6,7 @@ cat > $PREFIX/.messages.txt <<EOF
          conda-forge's singularity $PKG_VERSION POST-LINK INSTRUCTIONS
 -------------------------------------------------------------------------------
 
-In order to make full use of all singularity features you will need the system
+In order to make use of all singularity features you will need the system
 administrator to own and suid the singularity '*-suid' files.
 
 If you have full sudo access, you can do this with the following command:
