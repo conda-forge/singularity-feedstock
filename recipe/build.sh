7a56d5db19cbb5886f38ac96a37813bdb2c2524a
Chris Burr <christopher.burr@cern.ch>
2019-12-11 21:19:13 +0000

    Clean up and try adding macOS support

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,27 +3,12 @@ set -euf
 
 pushd src/github.com/sylabs/${PKG_NAME}
 
-# Create a C and CPP compiler for singularity
-cat > singularity-cc <<_EOF
-#!/usr/bin/env bash
-exec $CC -I${PREFIX}/include -L${PREFIX}/lib \$@
-_EOF
-chmod 755 singularity-cc
-
-cat > singularity-cxx <<_EOF
-#!/usr/bin/env bash
-exec ${CXX:-/bin/false}- -I${PREFIX}/include -L${PREFIX}/lib \$@
-_EOF
-chmod 755 singularity-cxx
-
 # configure
 ./mconfig \
   -P release-stripped \
   --without-suid \
   --without-network \
-  -p $PREFIX \
-  -c "${PWD}/singularity-cc" \
-  -x "${PWD}/singularity-cxx"
+  -p $PREFIX
 
 # build
 pushd builddir
@@ -32,9 +17,6 @@ make
 
 # install
 make install
-
-# Add post-install script with message on how to grant the -suid piece
-cp $RECIPE_DIR/post-link.sh $PREFIX/bin/.$PKG_NAME-post-link.sh
 
 # Make Empty session dir
 mkdir -p $PREFIX/var/singularity/mnt/session
