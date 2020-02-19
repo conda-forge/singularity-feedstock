d38184029424ff89d81fc1ae6d45cf846b5d664c
Chris Burr <chrisburr@users.noreply.github.com>
2020-02-19 05:21:11 +0100

    Avoid https://github.com/conda/conda-build/issues/2674

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -19,7 +19,8 @@ conda-build:
 
 CONDARC
 
-conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
+# Force older conda-build to avoid https://github.com/conda/conda-build/issues/2674
+conda install --yes --quiet conda-forge-ci-setup=2 "conda-build<3.17" -c conda-forge
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
