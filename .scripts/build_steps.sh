cf80df100eb4191c5ed3986d43795bc8b5e0fdbe
Chris Burr <chrisburr@users.noreply.github.com>
2020-07-23 10:11:22 +0200

    --suppress-variables isn't supported in older conda-build versions

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -30,7 +30,6 @@ source run_conda_forge_build_setup
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
-    --suppress-variables \
     --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
 validate_recipe_outputs "${FEEDSTOCK_NAME}"
 
