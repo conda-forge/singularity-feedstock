56d8000bd606d021eb79fa581e75a7aa6fda5f6f
ngam <67342040+ngam@users.noreply.github.com>
2021-11-14 21:58:42 -0500

    Update build_steps.sh

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -27,7 +27,7 @@ conda-build:
 CONDARC
 BUILD_CMD=build
 
-conda install --yes --quiet "conda-forge-ci-setup=3" conda-build pip ${GET_BOA:-} -c conda-forge
+conda install --yes --quiet "conda-forge-ci-setup=3" conda-build pip python ${GET_BOA:-} -c conda-forge
 
 # set up the condarc
 setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
